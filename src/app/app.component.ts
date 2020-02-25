24d04723c7125972bb5d4d798585f92cb7a4a70c
dina82 <dinaabdellatif94@gmail.com>
2020-02-25 14:34:17 +0200

    login update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   if(localStorage.getItem("currentUser") !== null){
     document.getElementById("login-btn").style.display = "none";
     document.getElementById("logout-btn").style.display = "inline";
+    document.getElementById("user__myTrip").style.display="inline"
     console.log("loyality");
   }
   }
